3c8dfb391e6b84b2f902cc20210e429ffe4f3574
MorganAU <augereau.morgan@laposte.net>
2014-06-11 17:35:41 +0200

    Add BarMenu, toolbar and actions

--- FenPrincipal.cpp
+++ FenPrincipal.cpp
@@ -2,17 +2,58 @@
 
 FenPrincipal::FenPrincipal()
 {
-    creerMenus();
+    QWebView *pageWeb = new QWebView(this);
+    pageWeb->load(QUrl("http://www.google.fr/"));
+    setCentralWidget(pageWeb);
+    showMaximized();
+
     creerActions();
+    creerMenus();
+    creerBarreDOutils();
+
 }
  void FenPrincipal::creerMenus()
  {
-     m_menuPages = new QMenu;
-     m_menuPages = menuBar()->addMenu("&Page");
+     m_fichier = new QMenu;
+     m_fichier = menuBar()->addMenu("F&ichier");
+     m_fichier->addAction(m_enregistrer);
+     m_fichier->addAction(m_rechercher);
+     m_fichier->addAction(m_imprimer);
+
+     m_navigation = new QMenu;
+     m_navigation = menuBar()->addMenu("&Navigation");
+     m_navigation->addAction(m_pageAccueil);
+     m_navigation->addAction(m_pagePrecedente);
+     m_navigation->addAction(m_pageSuivante);
+     m_navigation->addAction(m_actualiserPage);
+     m_navigation->addAction(m_stopLoad);
+
  }
 
  void FenPrincipal::creerActions()
  {
-     m_pagePrecedente = new QAction("Page pré&cédente", this);
-     m_menuPages->addAction(m_pagePrecedente);
+     m_enregistrer = new QAction(QIcon("Icones/save.png"),"Enregistrer la page sous...", this);
+     m_rechercher = new QAction(QIcon("Icones/search.png"),"Rechercher...", this);
+     m_imprimer = new QAction(QIcon("Icones/impr.png"), "Imprimer", this);
+
+     m_pagePrecedente = new QAction(QIcon("Icones/prec.png"), "Page pré&cédente", this);
+     m_pageSuivante = new QAction(QIcon("Icones/suiv.png"), "Page &suivante", this);
+     m_actualiserPage = new QAction(QIcon("Icones/actu.png"),"&Actualiser la page", this);
+     m_pageAccueil = new QAction(QIcon("Icones/home.png"),"&Page d'accueil", this);
+     m_stopLoad = new QAction(QIcon("Icones/stop.png"), "Arrêter le chargement", this);
+ }
+
+ void FenPrincipal::creerBarreDOutils()
+ {
+    m_toolBarFichier = new QToolBar;
+    m_toolBarFichier = addToolBar("Fichier");
+    m_toolBarFichier->addAction(m_enregistrer);
+    m_toolBarFichier->addAction(m_rechercher);
+    m_toolBarFichier->addAction(m_imprimer);
+    m_toolBarFichier->addAction(m_pagePrecedente);
+    m_toolBarFichier->addAction(m_pageSuivante);
+    m_toolBarFichier->addAction(m_actualiserPage);
+    m_toolBarFichier->addAction(m_pageAccueil);
+    m_toolBarFichier->addAction(m_stopLoad);
+
  }
--- FenPrincipal.h
+++ FenPrincipal.h
@@ -2,6 +2,8 @@
 #define FENPRINCIPAL_H
 
 #include <QtWidgets>
+#include <QWebView>
+
 
 
 class FenPrincipal : public QMainWindow
@@ -15,8 +17,19 @@ public:
     void creerBarreDOutils();
 
 private:
-    QMenu *m_menuPages;
+    QMenu *m_fichier;
+    QAction *m_enregistrer;
+    QAction *m_rechercher;
+    QAction *m_imprimer;
+
+    QMenu *m_navigation;
     QAction *m_pagePrecedente;
+    QAction *m_pageSuivante;
+    QAction *m_actualiserPage;
+    QAction *m_pageAccueil;
+    QAction *m_stopLoad;
+
+    QToolBar *m_toolBarFichier;
 };
 
 #endif // FENPRINCIPAL_H
