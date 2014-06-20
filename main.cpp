93740b96da18d87700f10ec8e460e49d1608520f
MorganAU <augereau.morgan@laposte.net>
2014-06-20 20:42:16 +0200

    add Qtranslator LibraryInfo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <QApplication>
-#include <QtWidgets>
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
 #include "FenPrincipal.h"
 
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    // Traduction des chaînes prédéfinies par Qt dans notre langue
+    QString locale = QLocale::system().name();
+    QTranslator translator;
+    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&translator);
+
+    // Ouverture de la fenêtre principale du navigateur
     FenPrincipal fenetre;
     fenetre.show();
 
